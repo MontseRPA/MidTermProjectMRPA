3dda13dca21d5bd506b7c24d15e7fc6afc99013e
MontseRPA <montse.pala@hotmail.com>
2024-02-29 19:28:40 +0100

    project con responsive mobil ok

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-console.log()
+var searchParams = new URLSearchParams(window.location.search)
+fetch(`https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects`).then(res => res.json()).then(data => console.log(data.find(pro => pro.uuid === searchParams.get(`id`))))
 
